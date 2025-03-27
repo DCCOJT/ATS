fa70e344bbf37ef9e2fb9e3150ed7f962993d97d
cristhel <cristhel@example.com>
2025-03-27 05:10:27 -0700

    add code

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $username = "root";
 $password = "";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
 
 if (!$conn) {
